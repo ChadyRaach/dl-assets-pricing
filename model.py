0232703bedc544c182c3c69191ece109cc2319cd
craach <chadyraach1997@gmail.com>
2021-11-13 22:22:09 +0100

    Implement train functions

--- model.py
+++ model.py
@@ -1,86 +1,88 @@
-import numpy as np
+import warnings
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+
 
 class DeepCharacteristics(nn.Module):
-    """
+    """ """
 
-    """
-    def __init__(self, n_layers, in_channels, out_channels, features, activation_type= 'relu', bias=True):
+    def __init__(self, n_layers, in_channels, out_channels, features, activation_type="relu", bias=True):
         super().__init__()
         self.layers = []
-        self.layers.append(DenseBlock(in_channels=in_channels,
-                                      out_channels=features,
-                                      activation_type=activation_type,
-                                      bias=bias))
-        #intermediate layers
-        for _ in range(n_layers-2):
-            self.layers.append(DenseBlock(in_channels=features,
-                                          out_channels=features,
-                                          activation_type=activation_type,
-                                          bias=bias))
-        #last layer
-        self.layers.append(DenseBlock(in_channels=features,
-                                          out_channels=out_channels,
-                                          activation_type=activation_type,
-                                          bias=bias))
+        self.layers.append(
+            DenseBlock(in_channels=in_channels, out_channels=features, activation_type=activation_type, bias=bias)
+        )
+        # intermediate layers
+        for _ in range(n_layers - 2):
+            self.layers.append(
+                DenseBlock(in_channels=features, out_channels=features, activation_type=activation_type, bias=bias)
+            )
+        # last layer
+        self.layers.append(
+            DenseBlock(in_channels=features, out_channels=out_channels, activation_type=activation_type, bias=bias)
+        )
         self.FC = nn.Sequential(*self.layers)
-    
-    def forward(self,Z):
+
+    def forward(self, Z):
         Y = self.FC(Z)
         return Y
 
-class DenseBlock(nn.Module):
 
-    def __init__(self, in_channels, out_channels, activation_type= 'relu', bias=True):
+class DenseBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, activation_type="relu", bias=True):
         super().__init__()
 
         self.linear = nn.Linear(in_channels, out_channels, bias=bias)
 
-        if activation_type == 'linear':
+        if activation_type == "linear":
             self.activate = nn.Identity()
-        elif activation_type == 'relu':
+        elif activation_type == "relu":
             self.activate = nn.ReLU(inplace=True)
-        elif activation_type == 'lrelu':
+        elif activation_type == "lrelu":
             self.activate = nn.LeakyReLU(inplace=True)
         else:
-            raise NotImplementedError(f'Not implemented activation function: '
-                                      f'`{activation_type}`!')
+            raise NotImplementedError(f"Not implemented activation function: " f"`{activation_type}`!")
+
     def forward(self, Z):
         Z = self.linear(Z)
         Z = self.activate(Z)
         return Z
 
+
 class SortedFactorModel(nn.Module):
-    def __init__(self, n_layers,
-                        in_channels,
-                        features,
-                        n_deep_factors,
-                        n_BM_factors,
-                        n_portfolio,
-                        activation_type= 'relu',
-                        bias=True):
+    def __init__(
+        self,
+        n_layers,
+        in_channels,
+        features,
+        n_deep_factors,
+        n_BM_factors,
+        n_portfolio,
+        activation_type="relu",
+        bias=True,
+        ranking_method="softmax",
+    ):
         super().__init__()
-        
         self.DC_network = DeepCharacteristics(n_layers, in_channels, n_deep_factors, features, activation_type, bias)
         self.beta = nn.Parameter(torch.randn(n_portfolio, n_deep_factors), requires_grad=True)
         self.gamma = nn.Parameter(torch.randn(n_portfolio, n_BM_factors), requires_grad=True)
-        self.register_parameter(name='gamma', param=self.gamma)
-        self.register_parameter(name='beta', param=self.beta)
+        self.register_parameter(name="gamma", param=self.gamma)
+        self.register_parameter(name="beta", param=self.beta)
+        self.ranking_method = ranking_method
 
     def forward(self, Z, r, g):
         """
         Args:
-            Z ([Tensor(MxK)]): firm characteristics
-            r ([Tensor(M)]): firm returns
-            g ([Tensor(D)]): benchmark factors
+            Z ([Tensor(T x M x K)]): firm characteristics
+            r ([Tensor(T x M)]): firm returns
+            g ([Tensor(T x D)]): benchmark factors
         """
+        if len(Z.size()) == 2:
+            Z = Z[None, :, :]
         Y = self.DC_network(Z)
-        print()
-        W = rank_weight(Y) # M x P \ P := n_deep_factors
-        f = torch.matmul(W.transpose(1, 2), r) # P x 1
+        W = rank_weight(Y, method=self.ranking_method)  # T x M x P \ P := n_deep_factors
+        f = torch.matmul(W.transpose(1, 2), r)  # T x P x 1
         R = torch.matmul(self.beta[None, :], f) + torch.matmul(self.gamma[None, :], g)
         return R
 
@@ -89,19 +91,41 @@ def rank_weight(Y, method="softmax"):
     """Applies the rank weight operation
 
     Args:
-        Y      ([Tensor(M x N)])
+        Y      ([Tensor(T x M x N)])
         method (string)
     """
-    eps = 1-6
-    mean = torch.mean(Y, axis=0)
-    var = torch.var(Y, axis=0)
-    normalised_data = (Y - mean) / (var + eps)
+    eps = 1 - 6
+    mean = torch.mean(Y, axis=1)
+    var = torch.var(Y, axis=1)
+    normalised_data = (Y - mean[:, None, :]) / (var[:, None, :] + eps)
     if method == "softmax":
         y_p = -50 * torch.exp(-5 * normalised_data)
         y_n = -50 * torch.exp(5 * normalised_data)
-        softmax = nn.Softmax(dim=1)
+        softmax = nn.Softmax(dim=2)
         W = softmax(y_p) - softmax(y_n)
-    if method == "equal_ranks":
+    elif method == "equal_ranks":
         pass
-        
+        M, P = Y.size()[-2], Y.size()[-1]
+        uniform_weight = 1 / (M // 3)
+        sorted, indices = torch.sort(Y, dim=1)
+        W = torch.zeros(Y.size())
+        for i in range(P):
+            W[indices.T[i][2 * M // 3:], i] = uniform_weight
+            W[indices.T[i][M // 3: 2 * M // 3], i] = 0
+            W[indices.T[i][: M // 3], i] = -uniform_weight
+    else:
+        warnings.warn(f"{method} not implemented yet. Softmax ranking will be applied.")
+        return rank_weight(Y, method="softmax")
+
     return W
+
+
+if __name__ == "__main__":
+    network = SortedFactorModel(2, 52, 32, 10, n_BM_factors=2, n_portfolio=5, ranking_method="softmax")
+    T = 100
+    M = 3000
+    fake_data = torch.rand(T, M, 52)
+    r = torch.rand(T, M, 1) * 10
+    g = torch.rand(T, 2, 1)
+    x = network(fake_data, r, g)
+    print(x.size())
---
+++ train.py
@@ -0,0 +1,167 @@
+from tqdm import tqdm
+import torch
+from model import SortedFactorModel
+from losses import pricing_error, time_series_variation
+
+
+def trainmodel(model, loss_fn, loader_train, loader_val=None,
+               optimizer=None, scheduler=None, num_epochs=1,
+               learning_rate=0.001, weight_decay=0.0, loss_every=10,
+               save_every=10, filename=None):
+    """
+    function that trains a network model
+    Args:
+        - model       : network to be trained
+        - loss_fn     : loss functions
+        - loader_train: dataloader for the training set
+        - loader_val  : dataloader for the validation set (default None)
+        - optimizer   : the gradient descent method (default None)
+        - scheduler   : handles the hyperparameters of the optimizer
+        - num_epoch   : number of training epochs
+        - learning_rate: learning rate (default 0.001)
+        - weight_decay: weight decay regularization (default 0.0)
+        - loss_every  : print the loss every n epochs
+        - save_every  : save the model every n epochs
+        - filename    : base filename for the saved models
+    Returns:
+        - model          : trained network
+        - loss_history   : history of loss values on the training set
+        - valloss_history: history of loss values on the validation set
+    """
+
+    dtype = torch.FloatTensor
+    # GPU
+    if torch.cuda.is_available():
+        model = model.cuda()
+        loss_fn = loss_fn.cuda()
+        dtype = torch.cuda.FloatTensor
+
+    if not(optimizer) or not(scheduler):
+        # Default optimizer and scheduler
+        optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate,
+                                      betas=(0.9, 0.999), eps=1e-08,
+                                      weight_decay=weight_decay, amsgrad=False)
+
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,
+                                                               mode='min', factor=0.8, patience=50,
+                                                               verbose=True, threshold=0.0001,
+                                                               threshold_mode='rel', cooldown=0,
+                                                               min_lr=0, eps=1e-08)
+
+    loss_history = []
+    valloss_history = []
+
+    # Display initial training and validation loss
+    message = ''
+    if loader_val is not None:
+        valloss = check_accuracy(model, loss_fn, loader_val)
+        message = ', val_loss = %.4f' % valloss.item()
+
+    print('Epoch %5d/%5d, ' % (0, num_epochs) +
+          'loss = %.4f%s' % (-1, message))
+
+    # Save initial results
+    if filename:
+        torch.save([model, optimizer, loss_history, valloss_history],
+                   filename+'%04d.pt' % 0)
+
+    # Main training loop
+    for epoch in tqdm(range(num_epochs)):
+
+        # The data loader iterates once over the whole data set
+        for (Z, r, g, R) in loader_train:
+            # make sure that the models is in train mode
+            model.train()
+
+            # Apply forward model and compute loss on the batch
+            Z = Z.type(dtype)  # Convert data into pytorch 'variables'
+            R = R.type(dtype)  # for computing the backprop of the loss
+            R_pred = model(Z, r, g)
+            loss = loss_fn(R_pred, R)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        # Store loss history to plot it later
+        loss_history.append(loss)
+        if loader_val is not None:
+            valloss = check_accuracy(model, loss_fn, loader_val)
+            valloss_history.append(valloss)
+
+        if ((epoch + 1) % loss_every == 0):
+            message = ''
+            if loader_val is not None:
+                message = ', val_loss = %.4f' % valloss.item()
+
+            print('Epoch %5d/%5d, ' % (epoch + 1, num_epochs) +
+                  'loss = %.4f%s' % (loss.item(), message))
+
+        # Save partial results
+        if filename and ((epoch + 1) % save_every == 0):
+            torch.save([model, optimizer, loss_history, valloss_history],
+                       filename+'%04d.pt' % (epoch + 1))
+            print('Epoch %5d/%5d, checkpoint saved' % (epoch + 1, num_epochs))
+
+        # scheduler update
+        scheduler.step(loss.data)
+
+    # Save last result
+    if filename:
+        torch.save([model, optimizer, loss_history, valloss_history],
+                   filename+'%04d.pt' % (epoch + 1))
+
+    return model, loss_history, valloss_history
+
+
+def check_accuracy(model, loss_fn, dataloader):
+    """
+    Auxiliary function that computes mean of the loss_fn
+    over the dataset given by dataloader.
+
+    Args:
+        - model: a network
+        - loss_fn: loss function
+        - dataloader: the validation data loader
+
+    Returns:
+        - loss over the validation set
+    """
+    import torch
+
+    dtype = torch.FloatTensor
+    if torch.cuda.is_available():
+        model = model.cuda()
+        loss_fn = loss_fn.cuda()
+        dtype = torch.cuda.FloatTensor
+
+    loss = 0
+    model.eval()  # set model to evaluation mode
+    with torch.no_grad():
+        for (x, y) in dataloader:
+            x_var = x.type(dtype)
+            y_var = y.type(dtype)
+            # apply model to x mini-batch
+            out = model(x_var)
+            # accumulate loss
+            loss += loss_fn(out, y_var)
+
+    # return loss divided by number of mini-batches
+    return loss/len(dataloader)
+
+
+if __name__ == "__main__":
+    T = 100
+    M = 3000
+    fake_data = torch.rand(T, M, 52)
+    r = torch.rand(T, M, 1) * 10
+    g = torch.rand(T, 2, 1)
+    R = torch.rand(T, 5, 1)
+    network = SortedFactorModel(5, 52, 32, 10, 2, 5)
+    lambda_ = torch.Tensor([0.1])
+
+    def loss(gt_returns, pred_returns):
+        return pricing_error(gt_returns, pred_returns) + lambda_ * time_series_variation(gt_returns, pred_returns)
+
+    dataloader = [[fake_data, r, g, R]]
+    trainmodel(network, loss,
+               dataloader, None, None, None, 100, weight_decay=0.1, loss_every=10)
