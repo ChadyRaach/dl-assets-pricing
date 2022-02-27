04dae4e9959303b3ff2e95b417f126b3ae34d9d8
craach <chadyraach1997@gmail.com>
2022-02-27 17:41:28 +0100

    Update training code

--- preprocess_data.py
+++ preprocess_data.py
@@ -20,6 +20,14 @@ def next_month(date: dt.datetime) -> dt.datetime:
     return date.replace(day=1)
 
 
+def last_day_of_month(any_day):
+    # this will never fail
+    # get close to the end of the month for any day, and add 4 days 'over'
+    next_month = any_day.replace(day=28) + dt.timedelta(days=4)
+    # subtract the number of remaining 'overage' days to get last day of current month, or said programattically said, the previous day of the first of next month
+    return next_month - dt.timedelta(days=next_month.day)
+
+
 def as_float(a: str) -> float:
     try:
         return float(a)
@@ -71,14 +79,17 @@ def transform_to_monthly_freq(data, start_month, end_month):
     return res
 
 
-class MyDataset(Dataset):
-    def __init__(self, firm_characteristics, firm_returns, benchmark_factors, portfolios_indexes, transform=None):
+class Dataset(Dataset):
+    def __init__(
+            self, firm_characteristics, firm_returns, benchmark_factors, portfolios_returns, transform=None,
+            start_date=None, end_date=None):
         self.firm_characteristics = torch.Tensor(firm_characteristics)
         self.firm_returns = torch.Tensor(firm_returns)
-        self.portfolios_indexes = portfolios_indexes
+        self.portfolios_returns = torch.Tensor(portfolios_returns)
         self.benchmark_factors = torch.Tensor(benchmark_factors)
-        self.temporal_relations()
-        self.transform = transform
+        self.start_date = start_date
+        self.end_date = end_date
+        self.lag_adjustment()
 
     def __getitem__(self, index):
         Z = self.firm_characteristics[index]
@@ -86,27 +97,36 @@ class MyDataset(Dataset):
         g = self.benchmark_factors[index]
         R = self.portfolios_returns[index]
 
-        if self.transform:
-            Z, r = self.transform(Z, r)
-
         return Z, r, g, R
 
     def __len__(self):
         return len(self.firm_characteristics)
 
-    def temporal_relations(self):
-        self.firm_characteristics = self.firm_characteristics[: -1]
-        portfolios_returns = []
-        for portfolio_indexes in self.portfolios_indexes:
-            portfolios_returns.append(torch.mean(self.firm_returns[1:, portfolio_indexes], axis=-1).view(-1, 1))
-        self.portfolios_returns = torch.concat(portfolios_returns, axis=1)
-        self.firm_returns = torch.Tensor(self.firm_returns)[:-1]
-        self.benchmark_factors = self.benchmark_factors[:-1]
+    def lag_adjustment(self):
+        self.firm_characteristics = self.firm_characteristics[:-1]
+        self.firm_returns = self.firm_returns[1:]
+        self.benchmark_factors = self.benchmark_factors[1:]
+        self.portfolios_returns = self.portfolios_returns[1:]
 
 
-def transform(Z, r):
-    p = torch.ones(Z.shape[0])
-    for i in range(Z.shape[1]):
-        p = p * (Z[:, i] != 0) * 1
-    indexes = p == 1
-    return Z[indexes], r[indexes]
+def train_val_split(ds, split_size=0.8):
+    n = ds.__len__()
+    assert 12 * (ds.end_date.year - ds.start_date.year) + (ds.end_date.month - ds.start_date.month) == n
+    val_size = int(n * (1 - split_size))
+    start_val = n - val_size
+    val_set = Dataset(ds[start_val:][0],
+                      ds[start_val:][1],
+                      ds[start_val:][2],
+                      ds[start_val:][3])
+    train_set = Dataset(ds[:start_val][0],
+                        ds[:start_val][1],
+                        ds[:start_val][2],
+                        ds[:start_val][3])
+    print(
+        f'Validation data on time range: {dt.datetime.strftime((ds.end_date - dt.timedelta(days=30*val_size)).replace(day=1), "%Y-%m-%d")} to {dt.datetime.strftime(last_day_of_month(ds.end_date), "%Y-%m-%d")}')
+
+    return train_set, val_set
+
+
+def batchify(dataset, batch_size):
+    return [dataset[i:i + batch_size] for i in range(len(dataset) - batch_size + 1)]
