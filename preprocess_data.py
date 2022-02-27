e9e007040b40696607376503da75c255531638c6
craach <chadyraach1997@gmail.com>
2022-02-27 17:47:50 +0100

    Remove deprecated code

--- preprocess_data.py
+++ preprocess_data.py
@@ -3,14 +3,6 @@ from dateutil import parser
 import pandas as pd
 import torch
 from torch.utils.data import Dataset
-import numpy as np
-
-CHAR_TO_FILE = {"assets": 'Total assets [Annual sliding Eq ref]',
-                "liabilities": 'Total liabilities [Annual sliding Eq ref]',
-                "closing_adj": 'Closing adj. by FX and div.',
-                "closing_ref": 'Closing ref.',
-                "market_cap_ref": 'Market cap ref.',
-                "market_cap": 'Market cap'}
 
 
 def next_month(date: dt.datetime) -> dt.datetime:
@@ -26,57 +18,6 @@ def last_day_of_month(any_day):
     next_month = any_day.replace(day=28) + dt.timedelta(days=4)
     # subtract the number of remaining 'overage' days to get last day of current month, or said programattically said, the previous day of the first of next month
     return next_month - dt.timedelta(days=next_month.day)
-
-
-def as_float(a: str) -> float:
-    try:
-        return float(a)
-    except Exception:
-        return None
-
-
-def get_latest_available_value(month: dt.datetime, df: pd.DataFrame, key: str) -> float:
-    subdf = df.copy()
-    subdf["dateTime"] = subdf.dateTime.apply(parser.parse)
-    subdf = subdf[subdf["dateTime"] < month].sort_values("perEndDate", ascending=False)
-    if len(subdf) > 0:
-        return subdf[key].iloc[0]
-    else:
-        return 0
-
-
-def average_monthly_value(month: dt.datetime, df: pd.DataFrame) -> float:
-
-    unvalid_key = df.keys()[0]
-    df["value"] = df[unvalid_key].apply(as_float)
-    df = df.drop(unvalid_key, axis=1)
-    df = df[df.value.notnull()]
-    df["dateTime"] = df.index
-    df["dateTime"] = df.dateTime.apply(parser.parse)
-    df = df[(df["dateTime"] >= month) & (df["dateTime"] < next_month(month))]
-    if len(df) > 0:
-        return df.value.mean()
-    else:
-        return 0
-
-
-def transform_to_monthly_freq(data, start_month, end_month):
-    res = {}
-    month = start_month
-    while month < end_month:
-        res[month] = {char: [] for char in CHAR_TO_FILE.keys()}
-        for firm in data.keys():
-            for char, file in CHAR_TO_FILE.items():
-                if char in ["assets", "liabilities"]:
-                    df = pd.DataFrame.from_dict(data[firm][file], orient="index")
-                    res[month][char].append(get_latest_available_value(month, df, "value"))
-                elif char in ["closing_adj", "closing_ref", "market_cap_ref", "market_cap"]:
-                    df = pd.DataFrame.from_dict(data[firm][file], orient="columns")
-                    if "Instrument" in df.index:
-                        df = df.drop("Instrument", axis=0)
-                    res[month][char].append(average_monthly_value(month, df))
-        month = next_month(month)
-    return res
 
 
 class Dataset(Dataset):
